b79e35231888bb9eec4c17869df750fc1f65e988
Nerubik <artem-soldatov00@mail.ru>
2020-10-31 18:01:28 +0300

    Pract9

---
+++ Pract9/src/ru/mirea/inbo02/pract9/inn/Main.java
@@ -0,0 +1,104 @@
+package ru.mirea.inbo02.pract9.inn;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class BadUserException extends IOException {
+    public BadUserException(String message) {
+        super(message);
+    }
+}
+
+class User {
+    private String name;
+    private String ID;
+
+    public User(String name, String ID) throws BadUserException {
+        checkID(ID);
+        this.name = name;
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    private void checkID(String currentID) throws BadUserException {
+        if (currentID.length() != 12) {
+            throw new BadUserException("Неверный ИНН!");
+        }
+        try {
+            Long.parseLong(currentID);
+        } catch (NumberFormatException e) {
+            throw new BadUserException("Неверный ИНН!");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Name = '" + name + '\'' +
+                ", ID = '" + ID + "'";
+    }
+}
+
+class Inn {
+    ArrayList<User> arrayList;
+
+    public Inn() {
+        arrayList = new ArrayList<>();
+    }
+
+    public void newUser(User user) throws BadUserException {
+        if(this.Contains(user)) {
+            throw new BadUserException("This ID is already in database: " + user.getID());
+        }
+        arrayList.add(user);
+    }
+
+    public void printDataBase() {
+        for(Object x : arrayList) {
+            System.out.println(x);
+        }
+    }
+
+    public boolean Contains(User x) {
+        for (User currentUser : arrayList) {
+            if(x.getID().equals(currentUser.getID())) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) throws BadUserException {
+        Inn inn = new Inn();
+
+        User user1 = new User("Andrey", "394829482951");
+        inn.newUser(user1);
+        User user2 = new User("Baga", "384928165014");
+        inn.newUser(user2);
+        User user3 = new User("Misha", "392948295928");
+        inn.newUser(user3);
+        User user4 = new User("Artem", "194829104829");
+        inn.newUser(user4);
+
+        inn.printDataBase();
+        if (inn.Contains(user3)) {
+            System.out.println("Пользователь " + user3.getName() + " находится в БД!");
+        } else {
+            System.out.println("Пользователь " + user3.getName() + " не находится в БД!");
+        }
+
+        Scanner scanner = new Scanner(System.in);
+        User user5 = new User(scanner.next(), scanner.next());
+        inn.newUser(user5);
+        inn.printDataBase();
+    }
+}
