b8c41ba690c66c840931a2c6c288deb4f274c176
Nerubik <artem-soldatov00@mail.ru>
2020-11-28 19:28:46 +0300

    Pract10

---
+++ Pract10/src/ru/mirea/inbo02/pract10/numb1/Main.java
@@ -0,0 +1,31 @@
+package ru.mirea.inbo02.pract10.numb1;
+
+interface ComplexAbstractFactory {
+    Complex createComplex();
+    Complex createComplex(int real, int image);
+}
+
+class ConcreteComplex implements ComplexAbstractFactory {
+
+    @Override
+    public Complex createComplex() {
+        return new Complex();
+    }
+
+    @Override
+    public Complex createComplex(int real, int image) {
+        return new Complex(real, image);
+    }
+}
+
+class Complex {
+    private int real;
+    private int image;
+
+    public Complex(int real, int image) {
+        this.real = real;
+        this.image = image;
+    }
+
+    public Complex() { }
+}
